e20f55f55ed4526734b2814e8ad9020744a78e8b
benjamin-montagnes <benji.montagnes@gmail.com>
2019-10-03 12:10:07 +0200

    exercise new branch

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+void greetingPalaiseau() {
+  std::cout << "Hello Palaiseau!" << std::endl;
+}
+
 int main() {
-    std::cout << "Hello Palaiseau!" << std::endl
-    return 0
+  greetingPalaiseau();
+  std::cout << "Hello Ecole Polytechnique!" << std::endl;
 }
